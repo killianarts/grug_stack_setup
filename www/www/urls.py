5c80f7e4563002312bb5acba992a958e80a82e32
Micah Killian <micah@killianarts.online>
2022-09-26 16:00:01 +0900

    Renamed contact to www
    Added render_block to INSTALLED_APPS

--- www/www/urls.py
+++ www/www/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path('', include('core.urls')),
+    path('', include('core.urls'))
 ]
